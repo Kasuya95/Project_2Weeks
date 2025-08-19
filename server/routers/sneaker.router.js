c297bf279097133f32b99a2df533e75165d83070
Phongsakorn <664259011@webmail.npru.ac.th>
2025-08-19 15:58:29 +0700

    api

--- server/routers/sneaker.router.js
+++ server/routers/sneaker.router.js
@@ -4,9 +4,14 @@ const sneakerController = require("../controllers/sneaker.controller.js");
 const router = express.Router();
 
 router.post("/", sneakerController.create);
+// localhost:3000/api/v1/
 router.get("/", sneakerController.getAll);
+// localhost:3000/api/v1/
 router.get("/:id", sneakerController.getById);
+// localhost:3000/api/v1/1
 router.put("/:id", sneakerController.update);
+// localhost:3000/api/v1/1
 router.delete("/:id", sneakerController.deleteById);
+// localhost:3000/api/v1/1
 
 module.exports = router;
