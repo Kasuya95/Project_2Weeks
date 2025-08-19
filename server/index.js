537293b430f5e83c989f196130653ca329d329d7
Phongsakorn <664259011@webmail.npru.ac.th>
2025-08-19 15:56:48 +0700

    cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const sneakerRouter = require("./routers/sneaker.router");
 
+app.use(
+  cors({
+    // origin ต้นทางมาจากไหนได้บ้าง
+    origin: ["http://localhost:5173", "127.0.0.1:5173"],
+    // อุญาติให้ ใช้ method ไรบ้าง หรือ service
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
